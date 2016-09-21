ab0ef15bbdfbbb188305270e1a5ba4775d4c6b54
anhkhoivu <anhkhoivu95@gmail.com>
2016-09-21 10:57:53 -0700

    Tweaked the output a bit,

--- CECS 444 - Lab Assignment 03/CECS 444 - Lab Assignment 03/Main.cpp
+++ CECS 444 - Lab Assignment 03/CECS 444 - Lab Assignment 03/Main.cpp
@@ -17,27 +17,41 @@ int main()
 	int j = 9;
 	int k;
 
+	// Display
+	cout << "************************************************************************************\n";
+	cout << "*                                                                                  *\n";
+	cout << "*      	     This program will compare ++i and j++ and what runs first.		   * \n";
+	cout << "*                                                                                  *\n";
+	cout << "************************************************************************************\n" << endl;
+
 	// Testing the program
 	cout << "Before: " << endl;
-	cout << "i is " << i << endl;
-	cout << "j is " << j << endl << endl;
-
-	k = f(++i) + g(j++) + j + 45;  // j++ runs before ++i
-
+	cout << "f(++i) is " << i << endl;
+	cout << "g(i++) is " << i << endl << endl;
 	cout << "After: " << endl;
-	cout << "i is " << i << endl;
-	cout << "j is " << j << endl;
+
+	//for(int n = 0; n < 2; n++)
+	k = f(++i) + g(i++) + j + 45;  // i++ runs before ++i on Visual Studio's C++compiler.
+								   // ++i runs before i++ on Clang's C++ compiler.
+	cout << "\nResult: " << endl;
+	cout << "k is " << k << endl;
 
 	// End program
 	cin.ignore(999, '\n');
 }
 
+// This method displays and returns i.
 int f(int num)
 {
+	// Displays result of integer i.
+	cout << "f(++i) is " << num << endl;
 	return num;
 }
 
+// This method displays and returns j.
 int g(int num)
 {
+	// Displays result of integer j.
+	cout << "g(i++) is " << num << endl;
 	return num;
 }
